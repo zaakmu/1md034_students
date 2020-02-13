8bc3458246af862553d3dfe4628e050d25367ecf
zaakmu <zainak98@yahoo.com>
2020-02-13 18:00:22 +0100

    added map instead of address input

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,57 +1,61 @@
 
 
-/*
+'use strict';
+const socket= io();
 
-function MenuItem (name, desc1, desc2, allergies,cal , image){
-this.name = name;
-this.desc1 = desc1;
-this.desc2 = desc2;
-this.cal = cal;
-this.allergies= allergies;
-this.image= image;
-this.info = function (){
-return this.name + ' ' + this.cal;
-};
-
-}
-
-let burg1 = new MenuItem('Flamin Hot Burger','Made with the highest quality meat' ,'Hot siracha sauce with bacon cheese and onions', 'contains gluten and sesame seeds' , '750 kCal' , 'img/cheeseburger.jpg' );
-
-let burg2 = new MenuItem ('Flamin ChickenBurger','Deep fried chicken filet' ,'flamin sauce with cabbage coleslaw', 'contains gluten and lactose', '450 kCal', 'img/chickenBurger.jpg' );
-
-let burg3 = new MenuItem ('Tangy FishBurger','Pan fried cod filet' ,'Tartar sauce with lettuce and tomotoes', ' contains gluten and lactose', '300 kCal', 'img/fishburger.jpg' );
-
-let burg4 = new MenuItem ('Hot Halloumi','Deep fried halloumi' ,'hot chili sauce with onions, tomotoes and ruccola', 'contains gluten and lactose', '400 kCal', 'img/halloumi.jpg' );
-
-let burg5 = new MenuItem ('Flamin DoubleCheese','Made with highest quality meat' ,'Meat burger with onion , cheddar and blue cheese', 'contains gluten and lactose', '900 kCal', 'img/doublecheese.jpg' );
-
-
-
-
-*/
 
 const vm = new Vue({
     el:'#info',
     data: {
+        orders:{},
         copiedMenu: food,
         burgerSelected: [],
         name: '',
         email: '',
-        street: '',
-        house: '',
         paymentStyle: 'Credit card',
         gender: 'do not wish',
         selected: [],
     },
-    methods:{
+    created: function() {
+        socket.on('initialize', function(data) {
+          this.orders = data.orders;
+        }.bind(this));
+    
+      
+        socket.on('currentQueue', function(data) {
+          this.orders = data.orders;
+        }.bind(this));
+      },
+      methods: {
         markDone: function(){
-            this.selected = [this.name,this.email,this.street,this.house,this.paymentStyle,this.gender,this.burgerSelected];
-            let order = document.getElementById("order").style.visibility = "visible";
+            this.selected = [this.name,this.email,this.paymentStyle,this.gender,this.burgerSelected]; 
+        },
 
-            console.log(this.selected);
-           
-        }
-    }
+        getNext: function() {
+         
+          let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+            return Math.max(last, next);
+          }, 0);
+          return lastOrder + 1;
+        },
+        addOrder: function(event) {
+         
+          let offset = {
+            x: event.currentTarget.getBoundingClientRect().left,
+            y: event.currentTarget.getBoundingClientRect().top,
+          };
+          socket.emit('addOrder', {
+            orderId: this.getNext(),
+            details: {
+              x: event.clientX - 10 - offset.x,
+              y: event.clientY - 10 - offset.y,
+            },
+            orderItems: ['Beans', 'Rice'],
+        });
+   
+        },
+    },
+
 });
 
 
